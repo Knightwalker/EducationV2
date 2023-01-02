c943eabb547cc31cabdf6a25d6ca3b15af409f91
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-01-02 23:25:16 +0200

    adds view components

---
+++ Projects/WeatherApp/WeatherApp/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models.Weather;
+
+namespace WeatherApp.ViewComponents
+{
+    public class CityViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeatherModel city)
+        {
+            this.ViewBag.CityClassName = GetCssClassByFahrenheit(city.TemperatureFahrenheit);
+            return View(city); //invokes view of the view component at Views/Shared/Components/Grid/Sample.cshtml
+        }
+
+        private string GetCssClassByFahrenheit(int temperatureFahrenheit)
+        {
+            return temperatureFahrenheit switch
+            {
+                (< 44) => "blue-back",
+                (>= 44) and (< 75) => "green-back",
+                (>= 75) => "orange-back"
+            };
+        }
+    }
+}
