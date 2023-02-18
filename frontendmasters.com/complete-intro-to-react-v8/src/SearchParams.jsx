c96f36a12ac69879fd5ef82d5020a70ced8bf5f2
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-02-18 23:20:21 +0200

    exercise: uncontrolled forms

--- frontendmasters.com/complete-intro-to-react-v8/src/SearchParams.jsx
+++ frontendmasters.com/complete-intro-to-react-v8/src/SearchParams.jsx
@@ -1,43 +1,60 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import Results from "./Results";
 import useBreedList from "./useBreedList";
-
+import fetchSearch from "./fetchSearch";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("");
+    const [requestParams, setRequestParams] = useState({
+        location: "",
+        animal: "",
+        breed: ""
+    });
+    // const [location, setLocation] = useState("");
     const [animal, setAnimal] = useState("");
-    const [breed, setBreed] = useState("");
-    const [pets, setPets] = useState([]);
+    // const [breed, setBreed] = useState("");
+    // const [pets, setPets] = useState([]);
     const [breeds] = useBreedList(animal);
 
-    useEffect(() => {
-        requestPets();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    const results = useQuery(["search", requestParams], fetchSearch);
+    const pets = results?.data?.pets ?? [];
 
-    async function requestPets() {
-        const res = await fetch(`https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`);
-        const json = await res.json();
+    // useEffect(() => {
+    //     requestPets();
+    //     // eslint-disable-next-line react-hooks/exhaustive-deps
+    // }, []);
 
-        setPets(json.pets);
-    }
+    // async function requestPets() {
+    //     const res = await fetch(`https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`);
+    //     const json = await res.json();
+
+    //     setPets(json.pets);
+    // }
 
     return (
         <div className="search-params">
             <form
                 onSubmit={e => {
                     e.preventDefault();
-                    requestPets();
+                    // requestPets();
+                    const formData = new FormData(e.target);
+                    const obj = {
+                        animal: formData.get("animal") ?? "",
+                        breed: formData.get("breed") ?? "",
+                        location: formData.get("location") ?? "",
+                    };
+                    setRequestParams(obj);
                 }}
             >
                 <label htmlFor="location">
                     Location
                     <input
                         id="location"
-                        value={location}
+                        name="location"
+                        // value={location}
                         placeholder="location"
-                        onChange={e => setLocation(e.target.value)}
+                        // onChange={e => setLocation(e.target.value)}
                     />
                 </label>
                 <label htmlFor="animal">
@@ -47,7 +64,7 @@ const SearchParams = () => {
                         value={animal}
                         onChange={e => {
                             setAnimal(e.target.value);
-                            setBreed("");
+                            // setBreed("");
                         }}
                     >
                         <option></option>
@@ -57,12 +74,13 @@ const SearchParams = () => {
                     </select>
                 </label>
                 <label htmlFor="breed">
-                    Bnimal
+                    Breed
                     <select
                         id="breed"
+                        name="breed"
                         disabled={breeds.length === 0}
-                        value={breed}
-                        onChange={e => setBreed(e.target.value)}
+                        // value={breed}
+                        // onChange={e => setBreed(e.target.value)}
                     >
                         <option></option>
                         {breeds.map((breed) => (
