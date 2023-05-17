4493e30066ecefb77afc476427f35eba7ce6b380
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-05-17 08:29:43 +0300

    exercise: starts with a fresh copy of finished intermediate-react-v5

--- frontendmasters.com/intermediate-react-v5/src/Results.jsx
+++ frontendmasters.com/intermediate-react-v5/src/Results.jsx
@@ -1,25 +1,27 @@
 import Pet from "./Pet";
 
 const Results = ({ pets }) => {
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {!pets.length ? (
-                <h1>No Pets Found</h1>
-            ) : (
-                pets.map(pet => (
-                    <Pet
-                        key={pet.id}
-                        id={pet.id}
-                        name={pet.name}
-                        animal={pet.animal}
-                        breed={pet.breed}
-                        images={pet.images}
-                        location={`${pet.city}, ${pet.state}`}
-                    />
-                ))
-            )}
-        </div>
-    );
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => {
+          return (
+            <Pet
+              animal={pet.animal}
+              key={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              id={pet.id}
+            />
+          );
+        })
+      )}
+    </div>
+  );
 };
 
 export default Results;
