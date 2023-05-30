96d69104cfe3581dfab20f961f9395910c84e3d2
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-05-30 20:44:18 +0300

    exercise: connecting redux into react

--- frontendmasters.com/redux-fundamentals/02_hooking_it_up_with_react/src/Counter.jsx
+++ frontendmasters.com/redux-fundamentals/02_hooking_it_up_with_react/src/Counter.jsx
@@ -1,16 +1,22 @@
+import { useSelector } from "react-redux";
+// import { set, increment, decrement } from "./actions";
+import { SetCounter } from "./SetCounter";
+import { actions } from "./store";
+
 export const Counter = () => {
-    const incident = 'Incident';
-    const count = 0;
+    const incident = "Incident";
+    const count = useSelector((state) => state.count);
   
     return (
       <main className="Counter">
         <h1>Days Since Last {incident}</h1>
         <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Reset</button>
-          <button>Decrement</button>
+          <button onClick={() => actions.increment()}>Increment</button>
+          <button onClick={() => actions.set(0)}>Reset</button>
+          <button onClick={() => actions.decrement()}>Decrement</button>
         </section>
+        <SetCounter />
       </main>
     );
   };
--- frontendmasters.com/redux-fundamentals/02_hooking_it_up_with_react/src/store.js
+++ frontendmasters.com/redux-fundamentals/02_hooking_it_up_with_react/src/store.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
+import { createStore, bindActionCreators } from "redux";
 import { reducer } from "./reducer";
+import { increment, decrement, set } from "./actions";
 
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 export const store = createStore(reducer, enhancer);
+export const actions = bindActionCreators({ increment, decrement, set}, store.dispatch);
