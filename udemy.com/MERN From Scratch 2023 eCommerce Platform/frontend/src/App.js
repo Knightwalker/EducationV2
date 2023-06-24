d2f77536a86aefa0e3854ac6a02d96305d3c4c9e
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-06-24 16:13:38 +0300

    feat(udemy.com): adds header, footer and product components

--- udemy.com/MERN From Scratch 2023 eCommerce Platform/frontend/src/App.js
+++ udemy.com/MERN From Scratch 2023 eCommerce Platform/frontend/src/App.js
@@ -1,6 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+
 const App = () => {
     return (
-        <h1>Welcome to ProShop</h1>
+        <>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <HomePage />
+                </Container>
+            </main>
+            <Footer />
+        </>
     )
 }
 
