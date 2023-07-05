bc7dabf34b1868d1b3d260dd8843fabb8a29e214
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-07-05 12:45:45 +0300

    feat: create loader and message components

--- udemy.com/MERN From Scratch 2023 eCommerce Platform/frontend/src/slices/apiSlice.js
+++ udemy.com/MERN From Scratch 2023 eCommerce Platform/frontend/src/slices/apiSlice.js
@@ -10,8 +10,17 @@ export const apiSlice = createApi({
                 url: PRODUCTS_URL
             }),
             keepUnusedDataFor: 5
+        }),
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url: `${PRODUCTS_URL}/${productId}`
+            }),
+            keepUnusedDataFor: 5
         })
     })
 });
 
-export const { useGetProductsQuery } = apiSlice;
+export const { 
+    useGetProductsQuery,
+    useGetProductDetailsQuery 
+} = apiSlice;
