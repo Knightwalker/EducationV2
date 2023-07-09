458bfae4f1fe75ad85bfed1a9cdb7f3fb1ebc825
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-07-09 18:05:25 +0300

    feat: add backend auth with jwt

--- udemy.com/MERN From Scratch 2023 eCommerce Platform/backend/src/index.js
+++ udemy.com/MERN From Scratch 2023 eCommerce Platform/backend/src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { config } from "dotenv";
+import cookieParser from "cookie-parser";
 config();
 import { connectDB } from "./configs/db.config.js";
-import productRoutes from "./routes/productRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 connectDB();
@@ -10,9 +12,13 @@ connectDB();
 const PORT = process.env.SERVER_PORT;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => { res.send("API is running..."); });
 app.use(productRoutes);
+app.use(userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
