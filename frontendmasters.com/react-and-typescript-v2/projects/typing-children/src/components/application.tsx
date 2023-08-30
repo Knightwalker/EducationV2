b3e5f361a7fbe544303ae2cfa62dd3f63aba921e
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-08-30 17:15:27 +0300

    exercise(frontendmasters.com): react with typescript

--- frontendmasters.com/react-and-typescript-v2/projects/typing-children/src/components/application.tsx
+++ frontendmasters.com/react-and-typescript-v2/projects/typing-children/src/components/application.tsx
@@ -8,35 +8,38 @@
  * React.ReactChild[];
  */
 
-type BoxProps = { children: any /* 👈 Get rid of this! */ };
-
-const Box = ({ children }: BoxProps) => {
-  return (
-    <section
-      className="m-4"
-      style={{ padding: '1em', border: '5px solid purple' }}
-    >
-      {children}
-    </section>
-  );
+type TBoxProps = {
+    children: React.ReactNode,
+    styles?: React.CSSProperties
 };
 
-const Application = () => {
-  return (
-    <main className="m-8">
-      <Box>
-        Just a string.
-        <p>Some HTML that is not nested.</p>
-        <Box>
-          <h2>Another React component with one child.</h2>
-        </Box>
-        <Box>
-          <h2 className="mb-4">A nested React component with two children.</h2>
-          <p>The second child.</p>
-        </Box>
-      </Box>
-    </main>
-  );
+const Box = ({ children, styles = { textAlign: "center" } }: TBoxProps): JSX.Element => {
+    return (
+        <section
+            className="m-4"
+            style={{ padding: '1em', border: '5px solid purple', ...styles }}
+        >
+            {children}
+        </section>
+    );
+};
+
+const Application = (): JSX.Element => {
+    return (
+        <main className="m-8">
+            <Box>
+                Just a string.
+                <p>Some HTML that is not nested.</p>
+                <Box>
+                    <h2>Another React component with one child.</h2>
+                </Box>
+                <Box>
+                    <h2 className="mb-4">A nested React component with two children.</h2>
+                    <p>The second child.</p>
+                </Box>
+            </Box>
+        </main>
+    );
 };
 
 export default Application;
