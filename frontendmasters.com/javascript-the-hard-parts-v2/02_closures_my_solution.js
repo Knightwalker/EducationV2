91254bc9435ea60d4024fd49c92350be7ada70bb
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-09-11 10:50:25 +0300

    exercise(frontendmasters.com): closure

--- frontendmasters.com/javascript-the-hard-parts-v2/02_closures_my_solution.js
+++ frontendmasters.com/javascript-the-hard-parts-v2/02_closures_my_solution.js
@@ -361,19 +361,36 @@ console.log(shouldCapitalizeLast(capLastAttempt2)); // => should log true
 
 // CHALLENGE 18
 function makeHistory(limit) {
+    const history = [];
 
+    function save(action) {
+        if (action === "undo") {
+            if (history.length >= 1) {
+                const item = history.pop();
+                return `${item} undone`;
+            }
+            return "nothing to undo";
+        } else {
+            if (history.length >= limit) {
+                history.shift();
+            }
+            history.push(action);
+            return `${action} done`;
+        }
+    }
+
+    return save;
 }
 
-// /*** Uncomment these to check your work! ***/
-// const myActions = makeHistory(2);
-// console.log(myActions('jump')); // => should log 'jump done'
-// console.log(myActions('undo')); // => should log 'jump undone'
-// console.log(myActions('walk')); // => should log 'walk done'
-// console.log(myActions('code')); // => should log 'code done'
-// console.log(myActions('pose')); // => should log 'pose done'
-// console.log(myActions('undo')); // => should log 'pose undone'
-// console.log(myActions('undo')); // => should log 'code undone'
-// console.log(myActions('undo')); // => should log 'nothing to undo'
+const myActions = makeHistory(2);
+console.log(myActions('jump')); // => should log 'jump done'
+console.log(myActions('undo')); // => should log 'jump undone'
+console.log(myActions('walk')); // => should log 'walk done'
+console.log(myActions('code')); // => should log 'code done'
+console.log(myActions('pose')); // => should log 'pose done'
+console.log(myActions('undo')); // => should log 'pose undone'
+console.log(myActions('undo')); // => should log 'code undone'
+console.log(myActions('undo')); // => should log 'nothing to undo'
 
 
 // CHALLENGE 19
