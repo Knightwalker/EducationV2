3221cfffcec8fd10f8c9fbe16a178d1cbb036dbd
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-09-14 11:55:14 +0300

    exercise(frontendmasters.com): asynchronicity

--- frontendmasters.com/javascript-the-hard-parts-v2/projects/03_asynchronous_javascript_my_solution.js
+++ frontendmasters.com/javascript-the-hard-parts-v2/projects/03_asynchronous_javascript_my_solution.js
@@ -65,25 +65,39 @@ limitedRepeat(); // should log (every second, for 5 seconds): hi for now
 
 /* CHALLENGE 6 */
 
-function everyXsecsForYsecs() {
-    // ADD CODE HERE
+function everyXsecsForYsecs(cb, interval, duration) {
+    const intervalId = setInterval(cb, interval);
+
+    setTimeout(() => {
+        clearInterval(intervalId);
+    }, duration);
 }
 // Uncomment the following lines to check your work!
-// function theEnd() {
-//   console.log('This is the end!');
-// }
-// everyXsecsForYsecs(theEnd, 2, 20); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+function theEnd() {
+  console.log('This is the end!');
+}
+everyXsecsForYsecs(theEnd, 2000, 20000); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
 
 
 /* CHALLENGE 7 */
 
 function delayCounter(target, wait) {
+    let counter = 0;
 
+    return function() {
+        let intervalId = setInterval(() => {
+            counter++;
+            console.log(counter);
+            if (counter === target) {
+                clearInterval(intervalId);
+            }
+        }, wait);
+    }
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-// const countLogger = delayCounter(3, 1000)
-// countLogger();
+const countLogger = delayCounter(3, 1000)
+countLogger();
 // After 1 second, log 1
 // After 2 seconds, log 2
 // After 3 seconds, log 3
@@ -91,12 +105,16 @@ function delayCounter(target, wait) {
 /* CHALLENGE 8 */
 
 function promised(val) {
-    // ADD CODE HERE
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(val);
+        }, 2000);
+    });
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-// const createPromise = promised('wait for it...');
-// createPromise.then((val) => console.log(val)); 
+const createPromise = promised('wait for it...');
+createPromise.then((val) => console.log(val)); 
 // will log "wait for it..." to the console after 2 seconds
 
 /* CHALLENGE 9 */
