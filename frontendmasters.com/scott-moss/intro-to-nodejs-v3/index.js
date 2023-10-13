8f7f89d639d1bd49685845c16144438cc18ac0fc
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-10-13 15:40:03 +0300

    exercise(frontendmasters.com): node cli

--- frontendmasters.com/scott-moss/intro-to-nodejs-v3/index.js
+++ frontendmasters.com/scott-moss/intro-to-nodejs-v3/index.js
@@ -1,19 +1,2 @@
 #!/usr/bin/env node
-import { count } from "./utils/funcs.js";
-import { readFile } from "node:fs/promises";
-
-import * as dns from "node:dns/promises";
-
-const note = process.argv[2];
-
-const newNote = {
-    id: Date.now(),
-    content: note
-}
-
-async function rf () {
-    const test = await dns.resolveNs("nodejs.org");
-    console.log(test);
-}
-
-rf();
+import "./src/command.js";
