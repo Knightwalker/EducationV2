87e9e91c17e90e025e034b26e63b3324aa36c908
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-11-12 13:32:15 +0200

    fs: clean tutorials i lost interest in and didnt finish

--- udemy.com/MERN From Scratch 2023 eCommerce Platform/backend/src/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getProducts, getProductById } from "../controllers/productController.js";
-const router = express.Router();
-
-router.get("/api/products", getProducts);
-router.get("/api/products/:id", getProductById);
-
-export default router;
