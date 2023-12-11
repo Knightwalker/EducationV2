971c8288c31586867086d17af2e985012efa0ee5
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-12-12 00:18:27 +0200

    exercise(udemy.com): c# with .net 7

--- udemy.com/harsha-vardhan/asp-net-core-7/02-getting-started/MyFirstApp/MyFirstApp/Program.cs
+++ udemy.com/harsha-vardhan/asp-net-core-7/02-getting-started/MyFirstApp/MyFirstApp/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    string path = context.Request.Path;
+    string method = context.Request.Method;
+    context.Response.StatusCode = 400;
+    await context.Response.WriteAsync("<h1>Hello World</h1>");
+    await context.Response.WriteAsync($"{path}");
+    await context.Response.WriteAsync($"{method}");
+});
 
 app.Run();
