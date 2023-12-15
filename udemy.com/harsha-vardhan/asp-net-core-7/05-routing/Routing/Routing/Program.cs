72927231c319aefcf07839e75993f3d06770518a
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-12-15 16:13:40 +0200

    exercise(udemy.com): c# with .net 7

--- udemy.com/harsha-vardhan/asp-net-core-7/05-routing/Routing/Routing/Program.cs
+++ udemy.com/harsha-vardhan/asp-net-core-7/05-routing/Routing/Routing/Program.cs
@@ -1,19 +1,19 @@
+using Microsoft.AspNetCore.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseRouting();
-
 #pragma warning disable ASP0014 // Suggest using top level route registrations
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapGet("/map1", async (context) => {
-        await context.Response.WriteAsync("In Map 1");
-    });
-    endpoints.MapPost("/map2", async (context) => {
-        await context.Response.WriteAsync("In Map 2");
+app.UseEndpoints(endpoints => {
+    endpoints.MapGet("/products/details/{id:int}", async (context) =>
+    {
+        int id = int.Parse(context.Request.RouteValues["id"]!.ToString()!);
+        await context.Response.WriteAsync(id.ToString());
     });
 });
 #pragma warning restore ASP0014 // Suggest using top level route registrations
+
 app.Run(async (context) =>
 {
     await context.Response.WriteAsync($"Request received at {context.Request.Path}");
