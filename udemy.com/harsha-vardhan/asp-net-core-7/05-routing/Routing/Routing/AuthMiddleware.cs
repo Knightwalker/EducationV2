2f3b9a85734bb54e9a90087034a313fe282e5686
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2023-12-17 22:42:17 +0200

    exercise(udemy.com): .net custom middleware

---
+++ udemy.com/harsha-vardhan/asp-net-core-7/05-routing/Routing/Routing/AuthMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Routing
+{
+    public class AuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            // Step 1: Map the URL you want to protect
+            if (httpContext.Request.Path.StartsWithSegments("/users"))
+            {
+                // Step 2: Apply your authentication logic here
+                bool isLoggedIn = false;
+                if (!isLoggedIn)
+                {
+                    httpContext.Response.StatusCode = 401; // Unauthorized
+                    return;
+                }
+            }
+
+            await _next(httpContext);
+        }
+    }
+
+    public static class AuthMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseAuthMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AuthMiddleware>();
+        }
+    }
+}
--- udemy.com/harsha-vardhan/asp-net-core-7/05-routing/Routing/Routing/Program.cs
+++ udemy.com/harsha-vardhan/asp-net-core-7/05-routing/Routing/Routing/Program.cs
@@ -1,15 +1,21 @@
 using Microsoft.AspNetCore.Http;
+using Routing;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseRouting();
+app.UseAuthMiddleware();
 #pragma warning disable ASP0014 // Suggest using top level route registrations
 app.UseEndpoints(endpoints => {
     endpoints.MapGet("/products/details/{id:int}", async (context) =>
     {
         int id = int.Parse(context.Request.RouteValues["id"]!.ToString()!);
         await context.Response.WriteAsync(id.ToString());
+    });
+    endpoints.MapGet("/users", async (context) =>
+    {
+        await context.Response.WriteAsync("Hello from users page");
     });
 });
 #pragma warning restore ASP0014 // Suggest using top level route registrations
