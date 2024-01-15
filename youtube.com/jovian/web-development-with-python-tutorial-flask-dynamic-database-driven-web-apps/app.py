f45ab125afc67754ba21ab0fa40500621330a0ae
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2024-01-15 18:32:03 +0200

    exercise(youtube.com): python, flask and jinja2

--- youtube.com/jovian/web-development-with-python-tutorial-flask-dynamic-database-driven-web-apps/app.py
+++ youtube.com/jovian/web-development-with-python-tutorial-flask-dynamic-database-driven-web-apps/app.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # Change host to 'localhost' and port to 5000
     app.run(host='localhost', port=5000, debug=True)
