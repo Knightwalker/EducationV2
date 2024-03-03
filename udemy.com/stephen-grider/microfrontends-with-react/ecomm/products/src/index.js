4bc7d5eee1cb7d9eb62ac648308aa90cac073f93
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2024-03-03 09:23:38 +0200

    exercise(udemy): microfrontends module federation

---
+++ udemy.com/stephen-grider/microfrontends-with-react/ecomm/products/src/index.js
@@ -0,0 +1,11 @@
+import faker from "faker";
+console.log("products");
+
+let products = '';
+
+for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName(0);
+    products += `<div>${name}</div>`;
+}
+
+document.querySelector("#dev-products").innerHTML = products;
