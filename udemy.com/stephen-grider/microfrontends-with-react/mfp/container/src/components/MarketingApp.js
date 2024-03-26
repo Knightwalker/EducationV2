de0ba330825e767a9cbdee8dd405b628a1cce23a
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2024-03-26 18:06:59 +0200

    exercise(udemy): microfrontends routing (#5)

--- udemy.com/stephen-grider/microfrontends-with-react/mfp/container/src/components/MarketingApp.js
+++ udemy.com/stephen-grider/microfrontends-with-react/mfp/container/src/components/MarketingApp.js
@@ -1,11 +1,23 @@
-import { mount } from "marketing/Marketing";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { mount } from "marketing/Marketing";
 
 const MarketingApp = () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: (location) => {
+                const nextPathname = location.pathname;
+                const currPathname = history.location.pathname;
+                if (currPathname === nextPathname) {
+                    return;
+                }
+                history.push(nextPathname);
+            }
+        });
+        history.listen(onParentNavigate);
     }, []);
 
     return (
