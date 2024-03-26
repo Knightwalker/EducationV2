f6fbc2970662b0c64e73d89a02aa353b62c81288
Knightwalker <37214263+Knightwalker@users.noreply.github.com>
2024-03-26 18:18:14 +0200

    exercise(udemy): microfrontends v2 (#5)

--- udemy.com/stephen-grider/microfrontends-with-react/mfp/marketing/src/bootstrap.js
+++ udemy.com/stephen-grider/microfrontends-with-react/mfp/marketing/src/bootstrap.js
@@ -1,16 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (el) => {
-    ReactDOM.render(<App/>, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history}/>, el);
+
+    return {
+        onParentNavigate(location) {
+            const nextPathname = location.pathname;
+            const currPathname = history.location.pathname;
+            if (currPathname === nextPathname) {
+                return;
+            }
+            history.push(nextPathname);
+        }
+    };
 };
 
 if (process.env.NODE_ENV === "development") {
     const root = document.querySelector("#_marketing-root");
 
     if (root) {
-        mount(root);
+        mount(root, { 
+            defaultHistory: createBrowserHistory() 
+        });
     }
 }
 
